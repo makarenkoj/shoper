f2ce16b56fb8fc462b6934d01af1debfb4ad8c6d
makarenkoj <makarenkoj53@gmail.com>
2021-07-12 17:28:41 +0300

    return

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,57 +1,69 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[ show update destroy ]
+  before_action :set_product, only: %i[ show edit update destroy ]
 
-  def add_to_cart
-    id = params[:id].to_i
-    session[:cart] << id unless session[:cart].include?(id)
-    redirect_to products_path
-  end
-
-  def remove_from_cart
-    id = params[:id].to_i
-    session[:cart].delete(id)
-    redirect_to products_path
-  end
-
+  # GET /products or /products.json
   def index
     @products = Product.all
   end
 
+  # GET /products/1 or /products/1.json
   def show
   end
 
+  # GET /products/new
   def new
     @product = Product.new
   end
 
+  # GET /products/1/edit
+  def edit
+  end
+
+  # POST /products or /products.json
   def create
     @product = Product.new(product_params)
-    if @product.save
-      redirect_to @product, notice: "Product was successfully created."
-    else
-      render :new, status: :unprocessable_entity
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: "Product was successfully created." }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
     end
   end
 
+  # PATCH/PUT /products/1 or /products/1.json
   def update
-    if @product.update(product_params)
-      redirect_to @product, notice: "Product was successfully updated."
-    else
-      render :edit, status: :unprocessable_entity
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product, notice: "Product was successfully updated." }
+        format.json { render :show, status: :ok, location: @product }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
     end
   end
 
+  # DELETE /products/1 or /products/1.json
   def destroy
     @product.destroy
-    redirect_to products_url, notice: "Product was successfully destroyed."
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
     end
 
+    # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :price, :price_cents, :currency)
+      params.require(:product).permit(:name, :price)
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,37 +1,9 @@
 class Product < ApplicationRecord
 
   validates :name, :price, presence: true
-  validates :price, numericality: {greater_than: 0, less_than: 10000000}
   
   def to_s
     name
   end
 
-  monetize :price, as: :price_cents
-
-  def to_builder
-    Jbuilder.new do |product|
-      product.price stripe_price_id
-      product.quantity 1
-    end
-  end
-
-  after_create do
-    product = Stripe::Product.create(name: name)
-    price = Stripe::Price.create(product: product, unit_amount: self.price, currency: self.currency)
-    update(stripe_product_id: product.id, stripe_price_id: price.id)
-  end
-
-  # stripe keeps products and prices separately. 
-  # updating local prices would require to update or create stripe prices.
-  # this is all additional complexity.
-  # better just create new products.
-
-  # after_update :create_and_assign_new_stripe_price, if: :saved_change_to_price?
-  # after_update :create_and_assign_new_stripe_price, if: :saved_change_to_currency?
-
-  # def create_and_assign_new_stripe_price
-  #   price = Stripe::Price.create(product: self.stripe_product_id, unit_amount: self.price, currency: self.currency)
-  #   update(stripe_price_id: price.id)
-  # end
 end
